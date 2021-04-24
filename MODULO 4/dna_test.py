e649fb46806cc3008632ce92a60ae13f32822cde
miguelmaguina <83112761+miguelmaguina@users.noreply.github.com>
2021-04-24 13:17:24 -0500

    Actualizado

--- MODULO 4/dna_test.py
+++ MODULO 4/dna_test.py
@@ -2,33 +2,33 @@ import pandas as pd
 import csv
 import re
 import os
-import sys
-df_small=pd.read_csv('./dna/databases/small.csv')
-df_large=pd.read_csv('./dna/databases/large.csv')
-def hallar_ADN():
-    n=int(input('Secuencia: '))
+def hallar_ADN(n: int):
     secuencia_n=open('./dna/sequences/'+str(n)+'.txt')
     with open('./dna/sequences/'+str(n)+'.txt','r') as secuencia_n:
         for linea in secuencia_n:
             linea
     ADN=linea
     return ADN
-def patron_small():    
-    ADN=hallar_ADN()
-    p_1=re.findall('AGATC',ADN)
-    p_2=re.findall('AATG',ADN)
-    p_3=re.findall('TATC',ADN)
-    for x in df_small['AGATC'].unique():
-        for y in df_small['AATG'].unique():
-            for z in df_small['TATC'].unique():
-                if x==len(p_1) and y==len(p_2) and z==len(p_3):
-                    if x==2 and y==8 and z==3:
-                        return 'Alice'
-                    elif x==4 and y==1 and z==5:
-                        return 'Bob'
-                    elif x==3 and y==2 and z==5:
-                        return 'Charlie'
-    return 'No Match'
+def patron_small(n:int):
+    df_small=pd.read_csv('./dna/databases/small.csv')
+    while True:
+        if n>0 and n<5:
+            ADN=hallar_ADN(n)
+            p_1=re.findall('AGATC',ADN)
+            p_2=re.findall('AATG',ADN)
+            p_3=re.findall('TATC',ADN)
+            for x in df_small['AGATC'].unique():
+                for y in df_small['AATG'].unique():
+                    for z in df_small['TATC'].unique():
+                        if x==len(p_1) and y==len(p_2) and z==len(p_3):
+                            if x==2 and y==8 and z==3:
+                                return 'Alice'
+                            elif x==4 and y==1 and z==5:
+                                return 'Bob'
+                            elif x==3 and y==2 and z==5:
+                                return 'Charlie'
+            return 'No Match'
+
 def count(cont, cadena):
     patron = rf'({cont})\1*'
     patron_general = re.compile(patron)
@@ -39,29 +39,40 @@ def count(cont, cadena):
             maximo = pareja[x].group().count(cont)
     return maximo
 
-def patron_large():    
-    ADN=hallar_ADN()
-    with open('./dna/databases/large.csv', 'r') as archivo_csv:
-        lector = csv.DictReader(archivo_csv)
-        diccionario = list(lector)
-    
-    AGATC = count("AGATC", ADN)
-    TTTTTTCT = count("TTTTTTCT", ADN)
-    TCTAG = count("TCTAG", ADN)
-    AATG = count("AATG", ADN)
-    GATA = count("GATA", ADN)
-    TATC = count("TATC", ADN)
-    GAAA = count("GAAA", ADN)
-    TCTG = count("TCTG", ADN)
+def patron_large(n:int):
+    while True:
+        if n>4 and n<21:    
+            ADN=hallar_ADN(n)
+            with open('./dna/databases/large.csv', 'r') as archivo_csv:
+                lector = csv.DictReader(archivo_csv)
+                diccionario = list(lector)
+            
+            AGATC = count("AGATC", ADN)
+            TTTTTTCT = count("TTTTTTCT", ADN)
+            TCTAG = count("TCTAG", ADN)
+            AATG = count("AATG", ADN)
+            GATA = count("GATA", ADN)
+            TATC = count("TATC", ADN)
+            GAAA = count("GAAA", ADN)
+            TCTG = count("TCTG", ADN)
 
-    for i in range(len(diccionario)):
-        if all([diccionario[i]["AGATC"] == str(AGATC), diccionario[i]["TTTTTTCT"] == str(TTTTTTCT), diccionario[i]["TCTAG"] == str(TCTAG), diccionario[i]["AATG"] == str(AATG), diccionario[i]["GATA"] == str(GATA), diccionario[i]["TATC"]== str(TATC), diccionario[i]["GAAA"] == str(GAAA), diccionario[i]["TCTG"] == str(TCTG)]):
-            name = diccionario[i]["name"]
-            break
+            for i in range(len(diccionario)):
+                if all([diccionario[i]["AGATC"] == str(AGATC), diccionario[i]["TTTTTTCT"] == str(TTTTTTCT), diccionario[i]["TCTAG"] == str(TCTAG), diccionario[i]["AATG"] == str(AATG), diccionario[i]["GATA"] == str(GATA), diccionario[i]["TATC"]== str(TATC), diccionario[i]["GAAA"] == str(GAAA), diccionario[i]["TCTG"] == str(TCTG)]):
+                    name = diccionario[i]["name"]
+                    break
+                else:
+                    name = "No match"
+            return name
+def definir_patron():
+    n=int(input('Secuencia: '))
+    while True:
+        if n>0 and n<5:
+            return patron_small(n)
+        elif n >4 and n<21:
+            return patron_large(n)
         else:
-            name = "No match"
-    return name
+            print('Secuencia no correcta')
+            n=int(input('Secuencia: '))
 
-#PROBAR PROGRAMA
-print(patron_small())
-print(patron_large())
+if __name__ == "__main__":           
+    print(definir_patron())
